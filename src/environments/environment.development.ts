74742b3f484d0040ef3d3d842a3d954ca2c76c09
kmrjyotigit <kmrjyotigit@gmail.com>
2026-01-06 09:13:46 +0530

    chore: switch to production API URLs for deployment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://localhost:7231/api/v1',
-    docApiUrl: 'https://localhost:7057/'
-    // baseUrl: 'https://api.travvellis.com/api/v1',
-    // docApiUrl: 'https://api.travvellis.com/'
+    // baseUrl: 'https://localhost:7231/api/v1',
+    // docApiUrl: 'https://localhost:7057/'
+    baseUrl: 'https://api.travvellis.com/api/v1',
+    docApiUrl: 'https://api.travvellis.com/'
 };
